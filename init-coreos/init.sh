206d82a103fb1fadca30c9ccf5af45729004ac52
wayofthepie <wayofthepie@gmail.com>
2015-07-02 21:55:50 +0000

    Wait for influxdb to start

--- init-coreos/init.sh
+++ init-coreos/init.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+# Wrapper around journalctl that allows timeout after x seconds.
+#
+# $1 the timeout in seconds to wait for journactl
+# $2 the service whose logs we want to tail
+journal_wrap () {
+    local timeout=${1}
+    local service=${2}
+    timeout --foreground ${timeout}s journalctl -u $service -f;
+}
+
+# Wait for a specific log entry to appear. If the
+wait_for_entry () {
+    local timeout=${1}
+    local service=${2}
+    local entry=${3}
+    local status=1
+
+    journalctl -u $service -f | while read -t ${timeout} line; do
+        echo ${line}
+        echo "Entry : ${entry}"
+        if [[ ${line} =~ ${entry} ]]; then
+            echo "Found entry"
+            # Vars set here do not propagate into function scope...
+            touch success.tmp
+            pkill -9 -P $$ journalctl
+            break
+        fi
+    done
+
+    # If status is 0 we have success, if not, fail.
+    if [ ! -f success.tmp ]; then
+        echo "Waiting for ${service} to log ${entry} timed out \\
+                after ${timeout} seconds!"
+        exit 1
+    fi
+
+    # Otherwise continue
+    echo "${service} successfully started, continuing ..."
+}
+
 launch () {
     local service=${1}
     fleetctl start ${service}
@@ -7,6 +47,7 @@ launch () {
 
 influxdb () {
     launch "influxdb.service"
+    wait_for_entry 600 influxdb '.*\{"status"\:"ok"\}.*'
 }
 
 cadvisor () {
